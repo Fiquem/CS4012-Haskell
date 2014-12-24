4a1ac83d1851f78432e90f7b084989103253f26a
Kevin Baker <eire32kevin@gmail.com>
2014-12-24 17:07:38 +0000

    Fixed and refactored the solver

--- Minesweeper/Solver.hs
+++ Minesweeper/Solver.hs
@@ -12,40 +12,44 @@ autoMove board difficulty =
   if fst result
     then snd result
     else board -- uncover board (head $ getUnrevealedCells board) -- reveal next unrevealed cell
-  where result = makeSafeMove board
+  where result = attemptSafeMove board
 
-makeSafeMove :: Board -> (Bool, Board)
-makeSafeMove board = attemptOneSafeMove board (getRevealedCells board) 
-
-attemptOneSafeMove :: Board -> [Cell] -> (Bool, Board)
-attemptOneSafeMove board [] = (False, board)
-attemptOneSafeMove board (x:xs) = 
+attemptSafeMove :: Board -> (Bool, Board)
+attemptSafeMove board = 
   if fst result
     then result
-    else attemptOneSafeMove board xs -- didn't find a safe move let's continue looking
-  where result = flagToAdjacent board x
+    else findOneSafeMove board uncoverAdjacent (getRevealedCells board)
+  where result = findOneSafeMove board flagAdjacent (getRevealedCells board) 
+
+findOneSafeMove :: Board -> (Board -> Cell -> (Bool, Board)) -> [Cell] -> (Bool, Board)
+findOneSafeMove board f [] = (False, board)
+findOneSafeMove board f (x:xs) = 
+  if fst result
+    then result
+    else findOneSafeMove board f xs -- didn't find a safe move let's continue looking
+  where result = f board x
 
 uncoverAdjacent :: Board -> Cell -> (Bool, Board)
-uncoverAdjacent board cell =
-  if (count == 0)
-    then (True, uncover board $ head unmarked)
+uncoverAdjacent board cell = searchAdjacent board cell uncover flagged unmarked 
+  where
+    flagged = flaggedAdjacentCells board cell
+    unmarked = unmarkedAdjacentCells board cell
+
+flagAdjacent :: Board -> Cell -> (Bool, Board)
+flagAdjacent board cell = searchAdjacent board cell flag unmarked unmarked 
+  where unmarked = unmarkedAdjacentCells board cell
+
+searchAdjacent :: Board -> Cell -> (Board -> Cell -> Board) -> [Cell] -> [Cell] -> (Bool, Board)
+searchAdjacent board cell f countList choiceList = 
+  if (count == (adjacentMines cell)) && ((length choiceList) > 0)
+    then (True, f board $ head choiceList)
     else (False, board)
   where
-    count = length flagged
-    unmarked = unmarkedAdjacentCells board cell
-    flagged = flaggedAdjacentCells board cell
+    count = length countList
 
-flagToAdjacent :: Board -> Cell -> (Bool, Board)
-flagToAdjacent board cell =
-  if (count <= (adjacentMines cell)) && (count > 0)
-    then (True, flag board $ head unmarked) -- apply only one move flag
-    else (False, board)
-  where 
-    count = length unmarked
-    unmarked = unmarkedAdjacentCells board cell
 
 unmarkedAdjacentCells :: Board -> Cell -> [Cell]
-unmarkedAdjacentCells board cell = filter (\x -> ((not $ revealed x) || (not $ flagged x))) $ getAdjacentCells board cell
+unmarkedAdjacentCells board cell = filter (\x -> ((not $ revealed x) && (not $ flagged x))) $ getAdjacentCells board cell
 
 flaggedAdjacentCells :: Board -> Cell -> [Cell]
 flaggedAdjacentCells board cell = filter (\x -> flagged x) $ getAdjacentCells board cell
