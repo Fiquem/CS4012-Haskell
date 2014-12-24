8523e5a3b2c2484db0d5c51580d3a232480b5762
Kevin Baker <eire32kevin@gmail.com>
2014-12-24 00:37:55 +0000

    Added some solver primitives

--- Minesweeper/Solver.hs
+++ Minesweeper/Solver.hs
@@ -7,3 +7,17 @@ import Minesweeper.Difficulty
 
 autoMove :: Board -> Difficulty -> Board
 autoMove board difficulty = board
+
+countNumberAdjacentHidden :: Board -> (Int, Int) -> Int
+countNumberAdjacentHidden board coord = count
+  where
+    count = sum (map (\x -> if revealed x then 0 else 1) adjacentCells)
+    adjacentCells = getCells adjacentCellsCoords
+    adjacentCellsCoords = listOfAdjacentTiles coord (length (board!!0)) (length board)
+
+filterAdjacentCoordsHidden :: Board -> (Int, Int) -> [(Int , Int)]
+filterAdjacentCoordsHidden board coord = filtered
+  where
+    filtered = filter (\x -> not (revealed (getCell board x))) adjacentCellsCoords
+    adjacentCellsCoords = listOfAdjacentTiles coord (length (board!!0)) (length board)
+
